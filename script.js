f6a59a74025e20a6dd34ab56822893e7a58f15c0
Bruna Keila <brunakeilaoliveira@gmail.com>
2023-07-13 23:15:16 -0300

    resolved.

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    let result = eval(display.value);
+    display.value = result;
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
